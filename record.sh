5c2569c4213b7e8dd4ce6f8ae3c1954f55bc271c
Justin Ethier <justin.ethier@gmail.com>
2016-05-02 22:27:47 -0400

    Updated for PL2

--- record.sh
+++ record.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # An example of how to encode a video
-prboom-plus -width 1280 -height 720 -iwad DOOM1.WAD -timedemo doom-e1m9-03.lmp -viddump doom-e1m9-03.mkv
+#prboom-plus -width 1280 -height 720 -iwad DOOM1.WAD -timedemo doom-e1m9-03.lmp -viddump doom-e1m9-03.mkv
+prboom-plus -width 1280 -height 720 -iwad PLUTONIA.WAD -file PL2.WAD -timedemo pl2-map19-01.lmp -viddump pl2-map19-01.mkv
