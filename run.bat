4856061e420bfb90056f87acde9e9f6ec86b5eed
Justin Ethier <justin.ethier@gmail.com>
2016-05-21 23:27:01 -0400

    Update run.bat
    Added -complevel 4 for final doom compatibility

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-prboom-plus -skill 4 -iwad PLUTONIA.WAD -file PL2.WAD -warp 19 -record tmp2\tmp.lmp
+REM prboom-plus -skill 4 -iwad PLUTONIA.WAD -file PL2.WAD -warp 19 -record tmp2\tmp.lmp
+prboom-plus -skill 4 -iwad PLUTONIA.WAD -file PL2.WAD -complevel 4 -warp 19 -record tmp2\tmp.lmp
