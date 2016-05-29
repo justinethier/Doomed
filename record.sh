e9b5812597200d1e912c50629a4dd1696ae65590
Justin Ethier <justin.ethier@gmail.com>
2016-05-28 21:09:08 -0400

    Recording

--- record.sh
+++ record.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 # An example of how to encode a video
 #prboom-plus -width 1280 -height 720 -iwad DOOM1.WAD -timedemo doom-e1m9-03.lmp -viddump doom-e1m9-03.mkv
-prboom-plus -width 1280 -height 720 -iwad PLUTONIA.WAD -complevel 4 -file PL2.WAD -timedemo pl2-map19-02.lmp -viddump pl2-map19-02.mkv
+
+# Plutonia Hecknology
+#prboom-plus -width 1280 -height 720 -iwad PLUTONIA.WAD -complevel 4 -file PL2.WAD -timedemo pl2-map19-02.lmp -viddump pl2-map19-02.mkv
+
+#
+prboom-plus -width 1280 -height 720 -iwad PLUTONIA.WAD -complevel 4 -timedemo plutonia/map-18-uv-speed-01.lmp -viddump plutonia/map-18-01.lmp
